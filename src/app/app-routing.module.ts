580d48a36bb7056f6c4109bf3ab390a89e684fb4
Daly Bouazizi <bouazizimohamedali@isimsf.u-sfax.tn>
2024-08-12 13:05:56 +0100

    inital commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import path from 'path';
+import { ManagementComponent } from './core/components/management/management.component';
+import { DashboardComponent } from './core/components/dashboard/dashboard.component';
+import { UserDashComponent } from './core/components/user-dash/user-dash.component';
+const routes: Routes = [
+  {path: 'management',
+    component: ManagementComponent,
+  },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Default route
+  {path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {path: 'UserCP',
+    component: UserDashComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { UserDashComponent } from './core/components/user-dash/user-dash.component';
+import { ManagementComponent } from './core/components/management/management.component';
+import { UsermanagementComponent } from './core/components/usermanagement/usermanagement.component';
+import { DashboardComponent } from './core/components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserDashComponent,
+    ManagementComponent,
+    UsermanagementComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
